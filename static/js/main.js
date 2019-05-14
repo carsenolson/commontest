7e15c4f3ac3b56bea2ef0c9cb07eb5dd8b5638c3
carsenolson <gashiloff@yandex.ru>
2019-05-14 10:10:56 +0300

    updated help and some fixes

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,10 @@
 function deleteRes(elem) {
 	console.log($(elem))
-	console.log({file_name: $(elem)[0].previousElementSibling.previousElementSibling.children[0].innerHTML, option:"delete"})
+	console.log({file_name: $(elem)[0].previousElementSibling.previousElementSibling.children[0].textContent, option:"delete"})
     $.ajax({
         method: "POST",
         url: window.location,
-        data: JSON.stringify({file_name: $(elem)[0].previousElementSibling.children[0].innerHTML}),
+        data: JSON.stringify({file_name: $(elem)[0].previousElementSibling.previousElementSibling.children[0].textContent, option:"delete"}),
         dataType: "json"
     })
     $($(elem)[0].parentNode).remove()
